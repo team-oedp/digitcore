b98a4fd28062add1bfa0606fb2b02edc042cc0f3
Henry Babbage <hbabbage@gmail.com>
2025-07-24 03:11:11 +0200

    Chore: deploy schemas

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,7 +4,7 @@
 **/
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'q0v6uag1'
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 
 export default defineCliConfig({ api: { projectId, dataset } })
