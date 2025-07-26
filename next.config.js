35e1cd9e505c61017aee79ad56ecf39c4ec3ceac
Henry Babbage <hbabbage@gmail.com>
2025-07-27 01:29:28 +0200

    add experimental config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+	experimental: {
+		viewTransition: true,
+		browserDebugInfoInTerminal: true,
+	},
+};
 
 export default config;
