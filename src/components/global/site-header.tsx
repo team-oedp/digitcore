aab0b2a36f1483d883561b362f11772e063234e9
Henry Babbage <hbabbage@gmail.com>
2025-08-08 11:38:58 +0200

    add header nav items back

--- src/components/global/site-header.tsx
+++ src/components/global/site-header.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Link, SidebarIcon } from "lucide-react";
+import { SidebarIcon } from "lucide-react";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "~/components/ui/button";
 import { Separator } from "~/components/ui/separator";
