3f6c427166a25d8105c4c4938ce64753d2c0e449
Henry Babbage <hbabbage@gmail.com>
2025-08-15 18:03:13 +0200

    Add PageHeader component and update global styles for typography
    - Introduced a new PageHeader component to display a title and description using CustomPortableText.
    - Updated globals.css to define new text styles for headings, buttons, badges, and pattern list item titles, enhancing overall typography consistency.

---
+++ src/components/shared/page-header.tsx
@@ -0,0 +1,14 @@
+import type { PortableTextBlock } from "next-sanity";
+import { CustomPortableText } from "../global/custom-portable-text";
+
+export function PageHeader({
+	title,
+	description,
+}: { title: string; description: PortableTextBlock[] }) {
+	return (
+		<header>
+			<h1 className="text-[32px] font-bold text-primary">{title}</h1>
+			<CustomPortableText value={description as PortableTextBlock[]} />
+		</header>
+	);
+}
