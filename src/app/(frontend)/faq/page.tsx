4abc3816af7837da55e598955eba14cbc20c6ad8
Henry Babbage <hbabbage@gmail.com>
2025-10-10 17:28:32 +0200

    update FAQ page with sanityFetch

--- src/app/(frontend)/faq/page.tsx
+++ src/app/(frontend)/faq/page.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
 import type { PortableTextBlock } from "next-sanity";
-import { draftMode } from "next/headers";
 import { FAQCategorySection } from "~/components/pages/faq/faq-category-section";
 import { UncategorizedFAQSection } from "~/components/pages/faq/uncategorized-faq-section";
 import { CustomPortableText } from "~/components/sanity/custom-portable-text";
 import { PageHeading } from "~/components/shared/page-heading";
 import { PageWrapper } from "~/components/shared/page-wrapper";
-import { client, sanityFetch } from "~/sanity/lib/client";
+import { sanityFetch } from "~/sanity/lib/client";
 import { FAQS_QUERY, FAQ_PAGE_QUERY } from "~/sanity/lib/queries";
-import { token } from "~/sanity/lib/token";
 import type {
 	FAQS_QUERYResult,
 	FAQ_PAGE_QUERYResult,
@@ -22,29 +20,15 @@ export const metadata: Metadata = {
 };
 
 export default async function FAQPage() {
-	const isDraftMode = (await draftMode()).isEnabled;
+	const pageData = (await sanityFetch({
+		query: FAQ_PAGE_QUERY,
+		revalidate: 60,
+	})) as FAQ_PAGE_QUERYResult | null;
 
-	const pageData = isDraftMode
-		? await client.fetch<FAQ_PAGE_QUERYResult | null>(
-				FAQ_PAGE_QUERY,
-				{},
-				{ perspective: "previewDrafts", stega: true, token },
-			)
-		: await sanityFetch<typeof FAQ_PAGE_QUERY>({
-				query: FAQ_PAGE_QUERY,
-				tags: ["faq-page"],
-			});
-
-	const faqs = isDraftMode
-		? await client.fetch<FAQS_QUERYResult | null>(
-				FAQS_QUERY,
-				{},
-				{ perspective: "previewDrafts", stega: true, token },
-			)
-		: await sanityFetch<typeof FAQS_QUERY>({
-				query: FAQS_QUERY,
-				tags: ["faq"],
-			});
+	const faqs = (await sanityFetch({
+		query: FAQS_QUERY,
+		revalidate: 60,
+	})) as FAQS_QUERYResult | null;
 
 	const { uncategorized, grouped } = faqs
 		? groupFaqsByCategory(faqs)
